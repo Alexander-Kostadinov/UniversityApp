01c4bb02ce834cdde424e2340dc1e6c5d6044bc9
Aleksandar Kostadinov <alekkostadinov@tu-sofia.bg>
2024-12-16 01:28:00 +0200

    Fix add_course form.

--- admin/add_course_form.php
+++ admin/add_course_form.php
@@ -24,18 +24,21 @@
 <table align="center" cellpadding="0" cellspacing="0" width="400">
 <form action="add_course.php" method="post" name="add_course">
 	<tr>
-		<td height="30" colspan="2"><p align="center" class="red14"><strong>Добавяне на нова дисциплина</strong></p></td>
+		<td height="30" colspan="2">
+			<p align="center" class="red14">
+				<strong>Добавяне на нова дисциплина</strong>
+			</p>
+		</td>
 	</tr>
 	<tr>
-		<td height="30"><p align="left" class="black14">Име *</p></td>
+		<td height="30"><p align="center">Име *</p></td>
 		<td><input type="text" name="name" size=40 class="field"></td>
 	</tr>
-	<tr>
-		<td height="30" align="right">
-			<input type="button" value="Добави" class="button" onClick="validate_form()"></td> 
-		<td><input type="reset" value="Отказ" class="button"></td> 
-	</tr>
-</form></table>
+</form>
+</table>
+	<input class="add-btn" type="button" value="Добави" class="button" onClick="validate_form()"
+		style="margin-top: 20px; font-size: 16px;">
+	<input class="cancel-btn" type="reset" value="Отказ" class="button" style="margin-top: 20px; font-size: 16px;">
 </body>
 </html>
 
